d26a8dafdeb5dd06054000825765f4107e2347a6
Emmanuel2813 <aroyewunemmanuel@gmail.com>
2026-07-20 09:55:13 +0100

    Initial Commit

--- Backend/mabelhomes_backend/settings.py
+++ Backend/mabelhomes_backend/settings.py
@@ -183,6 +183,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://www.mabelhomes.org",
+    "https://mabelhomes.org",
+    "https://admin.mabelhomes.org",
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.onrender\.com$",
